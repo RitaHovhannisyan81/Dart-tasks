43e837c6945c40fcf7ce3e3a6d736f369e094996
Rita <143283685+RitaHovhannisyan81@users.noreply.github.com>
2023-11-24 16:58:47 +0400

    Add files via upload

---
+++ lesson.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+//1
+// void main() {
+//   print("Enter username:");
+//   String? name = stdin.readLineSync();
+//   print("Hello $name");
+//
+//   double myWeight = 70;
+//   double myHeight = 1.75;
+//   double bmi = calculateBMI(double myWeight ,double myHeight);
+//   bmi= myWeight/(myHeight)**2
+//   print("Hello name. Your BMI is bmi.");
+//}
+//2
+// void main() {
+//   print("Enter first number:");
+//   int? firstNumber = int.parse(stdin.readLineSync()!);
+//   print("Enter second number:");
+//   int? secondNumber = int.parse(stdin.readLineSync()!);
+//   print("The summ of numbers is ${firstNumber + secondNumber}");
+// }
+//3
+
+// void main() {
+//   final a = 10; // An integer
+//   final b = "20"; // A string
+//   final c = int.parse(b);
+//   print(a + c);
+// }
+
+//4 ????
+// void main() {
+//   print("Enter the age of user:");
+//   int? age = int.parse(stdin.readLineSync());
+//   if (age < 18) {
+//     print("Minor");
+//   } else if (age >= 18) {
+//     print("Adult");
+//   } else {
+//     print("Unknown");
+//   }
+// }
+
+//5
+// void main() {
+//   int age = 30; //ete tariq@ 0 dnenq kvercni defolt arjeq@ zexch chi hashvi
+//   double price = 1500.00;
+//   int? discount; //zexj 60 tarekanic barcr
+//   if (age >= 60) {
+//     discount = 20;
+//   }
+//   discount = discount ?? 0;
+//   double finalPrice = price - discount;
+//   print(finalPrice);
+// }
+
+//8
+
+void main(List<String> argument) {
+  List<dynamic> myList = [6, 4, 4.5, "str"];
+}
